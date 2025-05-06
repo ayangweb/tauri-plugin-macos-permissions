f01ccbf624a6fcf4b938ce73294ed1f10be2d179
ayang <473033518@qq.com>
2025-05-06 19:31:52 +0800

    feat: support for checking and requesting input monitoring permission

--- src/commands.rs
+++ src/commands.rs
@@ -256,3 +256,56 @@ pub async fn request_camera_permission() -> Result<(), String> {
 
     Ok(())
 }
+
+/// Check input monitoring permission.
+///
+/// # Returns
+/// - `bool`: `true` if input monitoring permission is granted, `false` otherwise.
+///
+/// # Example
+/// ```
+/// use tauri_plugin_macos_permissions::check_input_monitoring_permission;
+///
+/// let authorized = check_input_monitoring_permission().await;
+/// println!("Authorized: {}", authorized); // false
+/// ```
+#[command]
+pub async fn check_input_monitoring_permission() -> bool {
+    #[cfg(target_os = "macos")]
+    {
+        #[link(name = "IOKit", kind = "framework")]
+        extern "C" {
+            fn IOHIDCheckAccess(request: u32) -> u32;
+        }
+
+        unsafe {
+            let status = IOHIDCheckAccess(1);
+
+            status == 0
+        }
+    }
+
+    #[cfg(not(target_os = "macos"))]
+    return true;
+}
+
+/// Request input monitoring permission.
+///
+/// # Example
+/// ```
+/// use tauri_plugin_macos_permissions::request_input_monitoring_permission;
+///
+/// request_input_monitoring_permission().await;
+/// ```
+#[command]
+pub async fn request_input_monitoring_permission() -> Result<(), String> {
+    #[cfg(target_os = "macos")]
+    {
+        Command::new("open")
+            .arg("x-apple.systempreferences:com.apple.preference.security?Privacy_ListenEvent")
+            .output()
+            .map_err(|error| error.to_string())?;
+    }
+
+    Ok(())
+}
